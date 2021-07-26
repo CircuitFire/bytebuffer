b2d95c753cc9540bdeb31717567362a7fd408fbd
CircuitFire <ced9088@gmail.com>
2021-07-25 21:05:27 -0700

    added comments

--- src/lib.rs
+++ src/lib.rs
@@ -1,48 +1,94 @@
+//! # ByteBuffer
+//! Allows for easy conversion of data to and from u8 iters.
+//! 
+//! ## Structs
+//! - ByteBuffer<T>
+//! 
+//! ## Enums
+//! - ByteErr
+//! 
+//! ## Traits
+//! - Bytes
+//! - WriteBytes
+
 use std::mem::size_of;
 use std::convert::TryInto;
-use std::io::Write;
+use std::io::{Write, Error};
+use std::fs::File;
 
-pub trait Bytes{
-    const STATIC_SIZE: bool;
-
-    fn into_bytes(&self) -> Box<dyn Iterator<Item = u8>>;
-
-    fn bytes_len(&self) -> usize;
-
-    fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ()> where Self: Sized;
+/// The collection of errors.
+#[derive(Debug)]
+pub enum ByteErr {
+    NeedMoreBytes,
+    InvalidBytes,
+    IoErr(Error),
 }
 
-pub struct ByteBuffer{
-    buffer: Vec<u8>,
+impl From<Error> for ByteErr {
+    fn from(err: Error) -> Self {
+        ByteErr::IoErr(err)
+    }
+}
+
+/// The Bytes trait allows for the easy conversion of data to and from u8 iters.
+pub trait Bytes{
+    /// Creates an iter over the bytes of self.
+    fn into_bytes(&self) -> Box<dyn Iterator<Item = u8>>;
+
+    /// Tries to create the indicated type out of an io byte iter.
+    fn from_io_bytes<T: Iterator<Item = Result<u8, Error>>>(bytes: &mut T) -> Result<Self, ByteErr> where Self: Sized;
+
+    /// Tries to create the indicated type out of a byte iter.
+    fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ByteErr> where Self: Sized;
+}
+
+/// The iter over the bytes for all of the basic types.
+pub struct ByteBuffer<T>{
+    buffer: T,
     current: usize,
 }
 
-impl ByteBuffer {
-    fn new(buffer: Vec<u8>) -> Box<ByteBuffer> {
-        Box::new(ByteBuffer{
-            buffer: buffer,
-            current: 0,
-        })
-    }
+trait MakeByteBuffer<T> {
+    fn new(buffer: T) -> Box<ByteBuffer<T>>;
 }
 
-impl Iterator for ByteBuffer {
-    type Item = u8;
-
-    fn next(&mut self) -> Option<Self::Item> {
-        if self.current < self.buffer.len() {
-            let result = Some(self.buffer[self.current]);
-            self.current += 1;
-            result
+macro_rules! impl_byte_buffer {
+    ($type:ty) => {
+        impl MakeByteBuffer<$type> for ByteBuffer<$type> {
+            fn new(buffer: $type) -> Box<ByteBuffer<$type>> {
+                Box::new(ByteBuffer{
+                    buffer: buffer,
+                    current: 0,
+                })
+            }
         }
-        else{
-            None
+        
+        impl Iterator for ByteBuffer<$type> {
+            type Item = u8;
+        
+            fn next(&mut self) -> Option<Self::Item> {
+                if self.current < self.buffer.len() {
+                    let result = Some(self.buffer[self.current]);
+                    self.current += 1;
+                    result
+                }
+                else{
+                    None
+                }
+            }
         }
-    }
+    };
 }
 
+impl_byte_buffer!([u8; 1]);
+impl_byte_buffer!([u8; 2]);
+impl_byte_buffer!([u8; 4]);
+impl_byte_buffer!([u8; 8]);
+impl_byte_buffer!([u8; 16]);
+
+/// Extension of the Write trait writing all bytes from a byte iter.
 pub trait WriteBytes: Write {
-    fn write_bytes<T: Iterator<Item = u8>>(&mut self, byte_iter: &mut T, buffer_size: usize) -> Result<(), std::io::Error>{
+    fn write_bytes<T: Iterator<Item = u8>>(&mut self, byte_iter: &mut T, buffer_size: usize) -> Result<(), Error>{
         let mut buffer: Vec<u8>;
 
         loop {
@@ -56,45 +102,46 @@ pub trait WriteBytes: Write {
     }
 }
 
+impl WriteBytes for File {}
+
 impl Bytes for bool{
-    const STATIC_SIZE: bool = true;
 
     fn into_bytes(&self) -> Box<dyn Iterator<Item = u8>>{
-        ByteBuffer::new(vec![*self as u8])
+        ByteBuffer::new([*self as u8])
     }
 
-    fn bytes_len(&self) -> usize{
-        size_of::<Self>()
+    fn from_io_bytes<T: Iterator<Item = Result<u8, Error>>>(bytes: &mut T) -> Result<Self, ByteErr> {
+        if let Some(byte) = bytes.next() {
+            Ok(byte? != 0)
+        }
+        else{
+            Err(ByteErr::NeedMoreBytes)
+        }
     }
 
-    fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ()> {
+    fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ByteErr> {
         if let Some(byte) = bytes.next(){
             Ok(byte != 0)
         }
         else{
-            Err(())
+            Err(ByteErr::NeedMoreBytes)
         }
     }
 }
 
 impl Bytes for char{
-    const STATIC_SIZE: bool = true;
 
     fn into_bytes(&self) -> Box<dyn Iterator<Item = u8>>{
         let temp = *self as u32;
-        ByteBuffer::new(temp.to_le_bytes().to_vec())
+        ByteBuffer::new(temp.to_le_bytes())
     }
 
-    fn bytes_len(&self) -> usize{
-        size_of::<Self>()
-    }
-
-    fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ()> {
-        let buffer = bytes.take(size_of::<Self>()).collect::<Vec<u8>>();
+    fn from_io_bytes<T: Iterator<Item = Result<u8, Error>>>(bytes: &mut T) -> Result<Self, ByteErr> {
+        let buffer = bytes.take(size_of::<Self>()).collect::<Result<Vec<_>, _>>()?;
 
         if buffer.len() != size_of::<Self>() {
             // not enough bytes
-            return Err(())
+            return Err(ByteErr::NeedMoreBytes)
         }
 
         let temp = u32::from_le_bytes(buffer.try_into().unwrap());
@@ -104,28 +151,50 @@ impl Bytes for char{
         }
         else {
             // invalid character
-            Err(())
+            Err(ByteErr::InvalidBytes)
+        }
+    }
+
+    fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ByteErr> {
+        let buffer = bytes.take(size_of::<Self>()).collect::<Vec<u8>>();
+
+        if buffer.len() != size_of::<Self>() {
+            // not enough bytes
+            return Err(ByteErr::NeedMoreBytes)
+        }
+
+        let temp = u32::from_le_bytes(buffer.try_into().unwrap());
+
+        if let Some(character) = std::char::from_u32(temp) {
+            return Ok(character)
+        }
+        else {
+            // invalid character
+            Err(ByteErr::InvalidBytes)
         }
     }
 }
 
 impl Bytes for u8{
-    const STATIC_SIZE: bool = true;
 
     fn into_bytes(&self) -> Box<dyn Iterator<Item = u8>>{
-        ByteBuffer::new(vec![*self])
+        ByteBuffer::new([*self])
     }
 
-    fn bytes_len(&self) -> usize{
-        size_of::<Self>()
+    fn from_io_bytes<T: Iterator<Item = Result<u8, Error>>>(bytes: &mut T) -> Result<Self, ByteErr> {
+        if let Some(byte) = bytes.next(){
+            Ok(byte?)
+        }
+        else{
+            Err(ByteErr::NeedMoreBytes)
+        }
     }
-
-    fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ()> {
+    fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ByteErr> {
         if let Some(byte) = bytes.next(){
             Ok(byte)
         }
         else{
-            Err(())
+            Err(ByteErr::NeedMoreBytes)
         }
     }
 }
@@ -133,22 +202,28 @@ impl Bytes for u8{
 macro_rules! impl_buffer {
     ($type:ty) => {
         impl Bytes for $type{
-            const STATIC_SIZE: bool = true;
 
             fn into_bytes(&self) -> Box<dyn Iterator<Item = u8>>{
-                ByteBuffer::new(self.to_le_bytes().to_vec())
+                ByteBuffer::new(self.to_le_bytes())
             }
         
-            fn bytes_len(&self) -> usize{
-                size_of::<Self>()
+            fn from_io_bytes<T: Iterator<Item = Result<u8, Error>>>(bytes: &mut T) -> Result<Self, ByteErr> {
+                let buffer = bytes.take(size_of::<Self>()).collect::<Result<Vec<_>, _>>()?;
+        
+                if buffer.len() != size_of::<Self>() {
+                    // not enough bytes
+                    return Err(ByteErr::NeedMoreBytes)
+                }
+        
+                Ok(<$type>::from_le_bytes(buffer.try_into().unwrap()))
             }
         
-            fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ()> {
+            fn from_bytes<T: Iterator<Item = u8>>(bytes: &mut T) -> Result<Self, ByteErr> {
                 let buffer = bytes.take(size_of::<Self>()).collect::<Vec<u8>>();
         
                 if buffer.len() != size_of::<Self>() {
                     // not enough bytes
-                    return Err(())
+                    return Err(ByteErr::NeedMoreBytes)
                 }
         
                 Ok(<$type>::from_le_bytes(buffer.try_into().unwrap()))
@@ -174,6 +249,8 @@ impl_buffer!(f64);
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::{fs, fs::File};
+    use std::io::Read;
 
     macro_rules! number_test {
         ($type:ty) => {
@@ -181,7 +258,7 @@ mod tests {
             println!("test min: {}", test);
             assert_eq!(test, <$type>::from_bytes(&mut test.into_bytes()).unwrap());
             let test = 75;
-            println!("test max: {}", test);
+            println!("test middle value: {}", test);
             assert_eq!(test, <$type>::from_bytes(&mut test.into_bytes()).unwrap());
             let test = <$type>::MAX;
             println!("test max: {}", test);
@@ -247,5 +324,17 @@ mod tests {
         assert_eq!(test, f64::from_bytes(&mut test.into_bytes()).unwrap());
         let test: f64 = -3.75;
         assert_eq!(test, f64::from_bytes(&mut test.into_bytes()).unwrap());
+    }
+
+    #[test]
+    fn read_write() -> Result<(), ByteErr> {
+        let mut file = File::create("test.txt")?;
+        let value: char = 'G'; 
+        file.write_bytes(&mut value.into_bytes(), 1024)?;
+        let file = File::open("test.txt")?;
+        assert_eq!(value, char::from_io_bytes(&mut file.bytes())?);
+        fs::remove_file("test.txt")?;
+
+        Ok(())
     }
 }
